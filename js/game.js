f064a706efb670fd7a9f4bee6661b488248f6e2e
Moxx1990 <schmidt-max@gmx.de>
2026-03-31 20:44:11 +0200

    Added Buttons and touchfunktion

--- js/game.js
+++ js/game.js
@@ -6,7 +6,8 @@ let keyboard = new Keyboard();
 function init() {
     canvas = document.getElementById("canvas");
     world = new World(canvas, keyboard);
-}
+    bindBtsPressEvents();
+};
 
 window.addEventListener("keydown", (e) => {
     if (e.key == "ArrowLeft") {
@@ -43,3 +44,46 @@ window.addEventListener("keyup", (e) => {
         keyboard.space = false;
     }
 });
+
+function bindBtsPressEvents() {
+    document.getElementById("btnLeft").addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        keyboard.left = true;
+    });
+    document.getElementById("btnLeft").addEventListener("touchend", (e) => {
+        e.preventDefault();
+        keyboard.left = false;
+    });
+    document.getElementById("btnRight").addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        keyboard.right = true;
+    });
+    document.getElementById("btnRight").addEventListener("touchend", (e) => {
+        e.preventDefault();
+        keyboard.right = false;
+    });
+    document.getElementById("btnUp").addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        keyboard.up = true;
+    });
+    document.getElementById("btnUp").addEventListener("touchend", (e) => {
+        e.preventDefault();
+        keyboard.up = false;
+    });
+    document.getElementById("btnDown").addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        keyboard.down = true;
+    });
+    document.getElementById("btnDown").addEventListener("touchend", (e) => {
+        e.preventDefault();
+        keyboard.down = false;
+    });
+    document.getElementById("btnSpace").addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        keyboard.space = true;
+    });
+    document.getElementById("btnSpace").addEventListener("touchend", (e) => {
+        e.preventDefault();
+        keyboard.space = false;
+    });
+};
